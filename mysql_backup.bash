747c873433e69633df31541dce1aabb3b6d36f2e
Nill Rinov <nill@ingolonde.pw>
2020-07-19 22:34:06 +0200

    Add scripts

---
+++ mysql_backup.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# This will dump all your databases
+# MySQL backup script
+# This script is licensed under GNU GPLv2+
+DATE=$(date +%Y%m%d%H%M)
+$BACKUP_PATH=/backup/mysql
+
+for DB in $(echo "show databases" | mysql --defaults-file=/etc/mysql/debian.cnf -N); do
+	mysqldump --defaults-file=/etc/mysql/debian.cnf $DB >$BACKUP_PATH/${DB}_${DATE}.sql
+
+	xz -e $BACKUP_PATH/${DB}_${DATE}.sql
+done
+
+# purge old dumps
+find $BACKUP_PATH/ -name "*.sql*" -mtime +8 -exec rm -vf {} \;
