5eeedf21684cf2773d48f0eeb5988bc92f4af1b0
Nill Rinov <47075922+Nill-R@users.noreply.github.com>
2021-04-18 15:27:52 +0200

    Small fixes

--- mysql_backup.bash
+++ mysql_backup.bash
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
+
 # This will dump all your databases
 # MySQL backup script
 # This script is licensed under GNU GPLv2+
+
 DATE=$(date +%Y%m%d%H%M)
 BACKUP_PATH=/backup/mysql
+MYSQLDUMP=$(which mysqldump)
+COMP=$(which zstd)
+
+if [ ! -f "$MYSQLDUMP" ]; then
+        printf "ERROR: The mysqldump not installed.\n"
+        printf "FIX: Please install mysqldump\n"
+        exit 1
+fi
+
+if [ ! -f "$COMP" ]; then
+        printf "ERROR: The zstd not installed.\n"
+        printf "FIX: Please install zstd\n"
+        exit 1
+fi
 
 for DB in $(echo "show databases" | mysql --defaults-file=/etc/mysql/debian.cnf -N); do
-	mysqldump --defaults-file=/etc/mysql/debian.cnf $DB >$BACKUP_PATH/${DB}_${DATE}.sql
+        $MYSQLDUMP --defaults-file=/etc/mysql/debian.cnf $DB >$BACKUP_PATH/${DB}_${DATE}.sql
 
-	zstd -19 --rm $BACKUP_PATH/${DB}_${DATE}.sql
+        $COMP -19 --rm $BACKUP_PATH/${DB}_${DATE}.sql
 done
 
 # purge old dumps
