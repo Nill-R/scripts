82ce7098983fdfd3e9605a32916832c5b199069d
Nill Rinov <nill@ingolonde.pw>
2021-04-18 16:48:02 +0200

    Small fixes

--- block_tor_exit_nodes.bash
+++ block_tor_exit_nodes.bash
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 
+IPSET=$(which ipset)
+IPTABLES=$(which iptables)
+
+if [ ! -f "$IPSET" ]; then
+	printf "ERROR: The ipset not installed.\n"
+	printf "FIX: Please install ipset\n"
+	printf "FIX: apt install ipset\n"
+	exit 1
+fi
+
+if [ ! -f "$IPTABLES" ]; then
+	printf "ERROR: The iptables not installed.\n"
+	printf "FIX: Please install iptables\n"
+	printf "FIX: apt install iptables\n"
+	exit 1
+fi
+
 # create a new set for individual IP addresses
-ipset -N tor iphash
+$IPSET -N tor iphash
 # get a list of Tor exit nodes that can access $YOUR_IP, skip the comments and read line by line
-wget -q https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=$(curl -4 ifconfig.co) -O - |sed '/^#/d' |while read IP
-do
-  # add each IP address to the new set, silencing the warnings for IPs that have already been added
-  ipset -q -A tor $IP
+wget -q https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=$(curl -4 https://ifconfig.co) -O - | sed '/^#/d' | while read IP; do
+	# add each IP address to the new set, silencing the warnings for IPs that have already been added
+	$IPSET -q -A tor $IP
 done
 # filter our new set in iptables
-iptables -A INPUT -m set --match-set tor src -j DROP
-
+$IPTABLES -A INPUT -m set --match-set tor src -j DROP
