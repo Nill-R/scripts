057ba51ebdec7548303317c11144a734e982d696
Nill Ringil <47075922+Nill-R@users.noreply.github.com>
2024-03-06 08:51:27 +0200

    Create update_containers.bash
    Create update_containers.bash

---
+++ update_containers.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Define the base directory
+base_dir="/opt/docker-compose/"
+
+# Get a list of subdirectories in the base directory
+subdirs=$(find "$base_dir" -mindepth 1 -maxdepth 1 -type d)
+
+# Loop through each subdirectory
+for subdir in $subdirs; do
+  echo "Processing directory: $subdir"
+  # Change to the subdirectory
+  cd "$subdir" || continue
+  # Pull the latest images, recreate containers, and prune old images
+  docker compose pull && docker compose up -d --force-recreate && docker image prune -a -f
+done
