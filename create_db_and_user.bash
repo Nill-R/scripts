3ba0b5544b15ba21aea3f54f76af3561479d3092
Nill Ringil <47075922+Nill-R@users.noreply.github.com>
2024-03-07 19:30:32 +0200

    Update create_db_and_user.bash
    New version

--- create_db_and_user.bash
+++ create_db_and_user.bash
@@ -3,18 +3,19 @@
 EXPECTED_ARGS=2
 E_BADARGS=65
 MYSQL=$(which mysql)
-PASS=$(tr -cd '[:alnum:]' </dev/urandom | fold -w24 | head -n1)
+PASS=$(openssl rand -base64 12)
 
 Q1="CREATE DATABASE IF NOT EXISTS $1;"
-Q2="GRANT USAGE ON *.* TO $2@localhost IDENTIFIED BY '$PASS';"
-Q3="GRANT ALL PRIVILEGES ON $1.* TO $2@localhost;"
+Q2="CREATE USER '$2'@'localhost' IDENTIFIED BY '$PASS';"
+Q3="GRANT ALL PRIVILEGES ON $1.* TO '$2'@'localhost';"
 Q4="FLUSH PRIVILEGES;"
 SQL="${Q1}${Q2}${Q3}${Q4}"
 
 if [ $# -ne $EXPECTED_ARGS ]; then
-	echo "Usage: $0 dbname dbuser"
-	exit $E_BADARGS
+    echo "Usage: $0 dbname dbuser"
+    exit $E_BADARGS
 fi
+
 $MYSQL -e "$SQL"
 
 printf "db name: %s\n" "$1"
