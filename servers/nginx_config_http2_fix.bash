c3c376e1fcfc2688dd347f957363d015186a2c46
Nill Ringil <nill@lindon.pw>
2026-02-26 06:17:29 +0400

    style(servers): remove inline comments from nginx http2 fix script

--- servers/nginx_config_http2_fix.bash
+++ servers/nginx_config_http2_fix.bash
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-2.0-or-later OR MIT
 
-# Директория с конфигами Nginx
 CONFIG_DIR="/etc/nginx/sites-available"
 
-# Проходим рекурсивно по директории
 find "$CONFIG_DIR" -type f -name "*.conf" | while read -r file; do
-    # Проверяем наличие старого формата строк
     if grep -qE '^\s*listen.*443.*ssl.*http2.*$' "$file"; then
         echo "Обрабатывается файл: $file"
         
-        # Удаляем `http2` из строк listen
         sed -i -E 's/(^\s*listen.*443.*ssl.*)http2(.*$)/\1\2/' "$file"
         sed -i -E 's/(^\s*listen.*\[::\]:443.*ssl.*)http2(.*$)/\1\2/' "$file"
         
-        # Добавляем `http2 on;` после второй директивы listen
         awk '
             /listen.*443.*ssl/ { 
                 print $0; 
